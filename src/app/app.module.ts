b086fc75227dc438d83177eab81b43647b6ceeae
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-01-13 09:54:30 +0800

    Created ReusableComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReusableComponent } from './reusable/reusable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReusableComponent
   ],
   imports: [
     BrowserModule,
