e29fcd4a8ff99c3054594421cf861b9aabdc225a
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-01-13 10:23:02 +0800

    Implemented ReusableComponent to array of names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-spec-resources';
+  title = 'Directives';
+  names = ['Juan', 'Jose', 'Pedro'];
 }
--- src/app/reusable/reusable.component.ts
+++ src/app/reusable/reusable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reusable',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reusable.component.css']
 })
 export class ReusableComponent implements OnInit {
+  @Input() name = '';
 
   constructor() { }
 
