57281f249e108bec075b61f9b68ed4afc0ca4f6d
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-01-19 12:56:20 +0800

    Removed the AppRoutingModue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReusableComponent } from './reusable/reusable.component';
 import { TemplateComponent } from './template/template.component';
@@ -20,7 +19,6 @@ import { ApiComponent } from './api/api.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
